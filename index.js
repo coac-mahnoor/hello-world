35bd050088fe63ba3f8566f37462b98f6e0da50a
coac-mahnoor <74734452+coac-mahnoor@users.noreply.github.com>
2021-05-18 02:16:16 +0500

    basic node js app

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-alert("Hello, World!");
+const express = require('express');
+const app = express()
+const port = 3100;
+
+app.get('/', (req, res) => res.send('Welcome!'));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
